aa4eca9fde702380b1d27f9a072b921898133800
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-24 14:56:34 +0200

    Add files via upload
    suomitest iso paketti test

---
+++ suomitest2.js
@@ -0,0 +1,10 @@
+var suomitest2 = [
+  {
+    "name": "Leikade Liiso",
+    "abc": "X:1\nT:Leikade Liiso\nL:1/8\nM:2/4\nI:linebreak $\nK:C exp ^F ^G\nV:1 treble nm= snm=\nV:1\n af aa/a/ | fd dd | ce ce | dd dd |$ af aa/a/ | fd dd | ce ce | dd df |:$ ag/f/ ff | gb aa | %10\n (ag/)f/ ge |1 dd df/f/ :|2 dd dd |] %13"
+  },
+  {
+    "name": "Polska från Loftahammar nuotti",
+    "abc": "X:1\nT:Polska från Loftahammar \nT:Trad. Sweden, Sandbyholm, Loftahammar, Småland efter Karl Andersson \nL:1/8\nQ:1/4=120\nM:3/4\nI:linebreak $\nK:G\nV:1 treble nm= snm=\nV:1\n B/c/B/A/ GA Bc | dd/e/ d2 B2 | Gg ge fg | a/g/f/e/ d4 |$ cA/c/ c/B/A/G/ FD | %5\n BB cc d2 | ed/c/ cB BA | AG G4 ::$ DG/F/ GB GB | %9\n BA/B/ c/B/A/G/ FD | DG/F/ GB gB | A/B/c/B/ A2 G2 |$ gB A/B/c/A/ BG | %13\n gB A/B/c/A/ BG | GA/B/ cA/c/ BA/G/ | F/G/A/F/ G2 G,2 ::$ %16\n d2 d/c/B/c/ dg | d2 d/c/B/c/ dg | gf/e/ dc BG | c2 B/c/d/B/ A2 |$ G,B G,B G,B | %21\n BA/B/ c/B/A/G/ FD | G,B G,B G,B |$ BA/B/ c/B/A/G/ FD | dd ef gB | %25\n c/B/c/A/ A2 G2 :| %26"
+  }
+    ];
