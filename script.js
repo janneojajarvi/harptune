db6e97cec86a8b0c63534e24bda0b5bf18f4f4e8
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-24 13:40:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ async function loadGistData() {
     "sessionSet17.js",
     "sessionSet18.js",
     "extrasetti5.js",
-    "suomitest1.js",
+    "suomitest2.js",
     "fsfolkdiktning01.js",
     "fsfolkdiktning02.js",
     "folkwikiSet1.js",
