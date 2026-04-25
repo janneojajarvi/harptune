76c60a534e1e9f2ca3d0a6809e713995cf8462c3
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-25 15:01:29 +0200

    Update esavelmat_kt1.js

--- esavelmat_kt1.js
+++ esavelmat_kt1.js
@@ -1,18 +1,18 @@
-var esavelmat_kt1 = [
+  var esavelmat_kt1 = [
     {
-        "name": "Nuorten miesten speli",
-        "abc": "X:1\nT:Nuorten miesten speli\nR:Nuotti-kirja\nS:Samuel Rinda-Nickola\nN:Vuosi 1809\nB:eSävelmät kt1_0001\nL:1/8\nM:3/4\nK:C\nB4 C5 C4 D4 E4 F#4 G4 A4 G4 F#4 G4 A4 B4 G4 B4 B4 C5 C4 D4 E4 F#4 G4 A4 G4 F#4 G4 G3 B4 C5 C4 D4 E4 F#4 G4 A4 G4 F#4 G4 A4 B4 G4 B4 B4 C5 C4 D4 E4 F#4 G4 A4 G4 F#4 G4 G3 E5 F5 E5 D5 E5 C5 C5 D5 E5 F5 E5 F5 E5 D5 C5 B4 G4 F5 E5 D5 E5 C5 C5 D5 E5 F5 E5 F5 D5 E5 C5 D5 E5 F5 G5 E5 G5 G5 F5 D5 F5 F5 E5 C5 E5 E5 D5 C5 B4 C5 C4 E5 F5 E5 D5 E5 C5 C5 D5 E5 F5 E5 F5 E5 D5 C5 B4 G4 F5 E5 D5 E5 C5 C5 D5 E5 F5 E5 F5 D5 E5 C5 D5 E5 F5 G5 E5 G5 G5 F5 D5 F5 F5 E5 C5 E5 E5 D5 C5 B4 C5 C4 G4 A4 B4 C5 D5 E5 C5 B4 A4 D5 C5 B4 A4 G4 A4 B4 C5 D5 E5 F5 E5 D5 C5 B4 C5 D5 D4 G4 A4 B4 C5 D5 E5 C5 B4 A4 D5 C5 B4 A4 G4 A4 B4 C5 D5 E5 F5 E5 D5 C5 B4 C5 D5 D4 E5 F5 E5 D5 E5 C5 C5 D5 E5 F5 E5 F5 E5 D5 C5 B4 G4 F5 E5 D5 E5 C5 C5 D5 E5 F5 E5 F5 D5 E5 C5 D5 E5 F5 G5 E5 G5 G5 F5 D5 F5 F5 E5 C5 E5 F5 D5 C5 B4 C5 C4 "
+        name: "Nuorten miesten speli",
+        abc: "%%abc-charset utf-8\\n\\nX:1000\\nT:Nuorten miesten speli\\nS:Samuel Rinda-Nickola\\nM:3/4\\nL:1/16\\nR:Nuotti-kirja\\nN:Vuosi 1809\\nB:eSävelmät kt1_0001\\nK:G\\nBc2CDE^FGA| G^FGABGBB| c2CDE^FGAG| ^FG2G,2Bc2CDE| ^FGAG^FGAB| GBBc2CDE^F| GAG^FG2G,2ef| edeccdef| efedcBGf| edeccdef| efdecdef| geggfdff| eceedcBc2| C2efedecc| defefedc| BGfedecc| defefdec| defgeggf| dffeceed| cBc2C2GABc| decBAdcB| AGABcdef| edcBcd2D2G| ABcdecBA| dcBAGABc| defedcBc| d2D2efedec| cdefefed| cBGfedec| cdefefde| cdefgegg| fdffecef| dcBc2C2 |]"
     },
     {
-        "name": "kt1_0002",
-        "abc": "X:2\nT:kt1_0002\nR:Nuotti-kirja\nS:Samuel Rinda-Nickola\nN:Vuosi 1809\nB:eSävelmät kt1_0002\nL:1/8\nM:3/4\nK:C\nG4 A4 B4 C5 C5 E5 D5 C5 C5 B4 B4 G4 A4 B4 C5 D5 E5 E5 D5 C5 C5 D5 D5 E5 D5 E5 F5 G5 F5 D5 F5 E5 D5 C5 B4 C5 E5 D5 E5 F5 G5 E5 D5 C5 E5 D5 C5 B4 C5 C4 G4 A4 B4 C5 C5 E5 D5 C5 C5 B4 B4 G4 A4 B4 C5 D5 E5 E5 D5 C5 C5 D5 D5 E5 D5 E5 F5 G5 F5 D5 F5 E5 D5 C5 B4 C5 E5 D5 E5 F5 G5 E5 D5 C5 E5 D5 C5 B4 C5 C4 E5 D5 E5 F5 G5 G5 F5 G5 F5 E5 D5 E5 D5 C5 D5 G5 A5 G5 F5 E5 E5 D5 C5 C5 D5 D5 E5 D5 E5 F5 G5 E5 D5 F5 E5 D5 C5 B4 C5 E5 D5 E5 G5 F5 E5 D5 C5 E5 D5 C5 B4 C5 C4 E5 D5 E5 F5 G5 G5 F5 G5 F5 E5 D5 E5 D5 C5 D5 G5 A5 G5 F5 E5 E5 D5 C5 C5 D5 D5 E5 D5 E5 F5 G5 E5 D5 F5 E5 D5 C5 B4 C5 E5 D5 E5 G5 F5 E5 D5 C5 E5 D5 C5 B4 C5 C4 "
+        name: "kt1_0002",
+        abc: "%%abc-charset utf-8\\n\\nX:1001\\nT:kt1_0002\\nS:Samuel Rinda-Nickola\\nM:3/4\\nL:1/16\\nR:Nuotti-kirja\\nN:Vuosi 1809\\nB:eSävelmät kt1_0002\\nK:G\\nGA[BB]c2c2edc| cB2B2GABc2d| eedccdd2e| defgfdfe| dcB2c2edef| gedcedcB| c2C2GA[BB]c2c2e| dccB2B2GAB| c2deedccd| d2edefgfd| fedcB2c2ed| efgedced| cBc2C2edef| g2gfgfede| dcd2g2agfe| edccdd2ed| efgedfed| cB2c2edegf| edcedcBc2| C2edefg2gf| gfededcd2| g2agfeedc| cdd2edefg| edfedcB2c2| edegfedc| edcBc2C2 |]"
     },
     {
-        "name": "kt1_0003",
-        "abc": "X:3\nT:kt1_0003\nR:Nuotti-kirja\nS:Samuel Rinda-Nickola\nN:Vuosi 1809\nB:eSävelmät kt1_0003\nL:1/8\nM:3/4\nK:C\nC5 D5 E5 F5 G5 G5 E5 F5 F5 E5 F5 G5 A5 G5 F5 E5 F5 E5 E5 F5 D5 D5 E5 C5 C5 B4 B4 G4 A4 G4 A4 B4 G4 C5 C5 D5 C5 B4 G4 C5 D5 E5 F5 E5 F5 D5 G5 A5 G5 F5 E5 F5 E5 D5 E5 C5 E5 D5 C5 B4 C5 C4 C5 D5 E5 F5 G5 G5 E5 F5 F5 E5 F5 G5 A5 G5 F5 E5 F5 E5 E5 F5 D5 D5 E5 C5 C5 B4 B4 G4 A4 G4 A4 B4 G4 C5 C5 D5 C5 B4 G4 C5 D5 E5 F5 E5 F5 D5 G5 A5 G5 F5 E5 F5 E5 D5 E5 C5 E5 D5 C5 B4 C5 C4 E5 C5 C5 E5 E5 G5 D5 C5 B4 A4 G4 A4 B4 C5 D5 E5 C5 C5 B4 A4 A4 C5 B4 G4 B4 A4 G4 F#4 G4 D4 B3 G3 G4 G4 F4 E4 C4 E4 F4 G4 G4 A4 A4 B4 B4 G4 C5 G4 G4 F4 E4 C4 E4 F4 G4 G4 A4 A4 B4 B4 G4 C5 C5 D5 E5 D5 C5 B4 C5 D5 E5 F5 E5 D5 G5 A5 G5 F5 E5 F5 E5 D5 E5 C5 E5 D5 C5 B4 C5 C4 E5 C5 C5 E5 E5 G5 D5 C5 B4 A4 G4 A4 B4 C5 D5 E5 C5 C5 B4 A4 A4 C5 B4 G4 B4 A4 G4 F#4 G4 D4 B3 G3 G4 G4 F4 E4 C4 E4 F4 G4 G4 A4 A4 B4 B4 G4 C5 G4 G4 F4 E4 C4 E4 F4 G4 G4 A4 A4 B4 B4 G4 C5 C5 D5 E5 D5 C5 B4 C5 D5 E5 F5 E5 D5 G5 A5 G5 F5 E5 F5 E5 D5 E5 C5 E5 D5 C5 B4 C5 C4 "
+        name: "kt1_0003",
+        abc: "%%abc-charset utf-8\\n\\nX:1002\\nT:kt1_0003\\nS:Samuel Rinda-Nickola\\nM:3/4\\nL:1/16\\nR:Nuotti-kirja\\nN:Vuosi 1809\\nB:eSävelmät kt1_0003\\nK:G\\ncdefggef| fefgagfe| f2eefddec| cBBGAGAB| G2ccdcBGc2| defefdg2a| gfefedec| edcBc2C2cd| efggeffe| fgagfef2e| efddeccB| BGAGABG2c| cdcBGc2de| fefdg2agf| efedeced| cBc2C2ecce| egdcBAGA| BcdeccBA| AcBGBAG^F| GDB,G,GGFE| CEFGGAAB| BGc2GGFEC| EFGGAABB| Gc2cdedcB| c2defedg2a| gfefedec| edcBc2C2ec| ceegdcBA| GABcdecc| BAAcBGBA| G^FGDB,G,GG| FECEFGGA| ABBGc2GGF| ECEFGGAA| BBGc2cded| cBc2defed| g2agfefed| ecedcBc2C2 |]"
     },
     {
-        "name": "Morsianspeli pijkains kansa",
-        "abc": "X:4\nT:Morsianspeli pijkains kansa\nR:Nuotti-kirja\nS:Samuel Rinda-Nickola\nN:Vuosi 1809\nB:eSävelmät kt1_0004\nL:1/8\nM:3/4\nK:C\nC5 D5 E5 F5 F5 E5 E5 D5 C5 B4 A4 B4 G4 G5 F5 G5 A5 A5 G5 G5 F5 E5 D5 F5 E5 C5 C5 C5 D5 E5 F5 F5 E5 E5 D5 C5 B4 A4 B4 G4 G5 F5 G5 A5 A5 G5 G5 F5 E5 D5 F5 E5 C5 C5 G5 F5 G5 F5 D5 F5 E5 C5 F5 D5 E5 F5 E5 E5 D5 C5 B4 C5 D5 E5 F5 E5 D5 C5 E5 D5 C5 B4 C5 C4 G5 F5 G5 F5 D5 F5 E5 C5 F5 D5 E5 F5 E5 E5 D5 C5 B4 C5 D5 E5 F5 E5 D5 C5 E5 D5 C5 B4 C5 C4 "
+        name: "Morsianspeli pijkains kansa",
+        abc: "%%abc-charset utf-8\\n\\nX:1003\\nT:Morsianspeli pijkains kansa\\nS:Samuel Rinda-Nickola\\nM:3/4\\nL:1/16\\nR:Nuotti-kirja\\nN:Vuosi 1809\\nB:eSävelmät kt1_0004\\nK:G\\ncdeffeed| cBABGgfg| aaggfedf| ecc2cdeff| eedcBABG| gfgaaggf| edfecc2gf| gfdfecfd| efe[ce]d2cBc| defedced| cBc2C2gfgf| dfecfdef| e[ce]d2cBcde| fedcedcB| c2C2 |]"
     }
 ];
