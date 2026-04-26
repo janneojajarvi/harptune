0f1a61d5c73142d7444112bcc7189eb2c80baa5f
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-26 19:52:20 +0200

    Add files via upload

---
+++ esavelmat_kansantanssit2.js
@@ -0,0 +1,6 @@
+var esavelmat_kansantanssit = [
+  {
+    name: "Nuorten miesten skeli",
+    abc: "X:1\nT:Nuorten miesten speli\nO:-()\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0001\nK:C\n|: {B}c4 C2D2 E2^F2 | GAG^F G2A2 B2GB | {B}c4 C2D2 E2^F2 | GAG^F G4 G,4:||: efed e2c2 c2de | fefe d2c2 B2G2 | f2ed e2c2 c2de | fefd e2c2 d2ef | g2eg gfdf fece | edcB c4 C4:||: G2AB c2d2 e2c2 | B2A2 d2c2 B2A2 | G2AB c2de fedc | B2c2 d4 D4:| | efed e2c2 c2de | fefe d2c2 B2G2 | f2ed e2c2 c2de | fefd e2c2 d2ef | g2eg gfdf fece | fdcB c4 C4:|"
+  }
+  ];
