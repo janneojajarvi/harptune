6d6fdb7b12ce3a30ccdd23f961510287dba43dd2
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-26 15:39:41 +0200

    Update esavelmat_setti1.js

--- esavelmat_setti1.js
+++ esavelmat_setti1.js
@@ -1,18 +1,18 @@
 var esavelmat_setti1 = [
   {
     name: "Nuorten miesten speli",
-    abc: "%%abc-charset utf-8\nX:1000\nT:Nuorten miesten speli\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0001\nK:G\n|: B | c4 C2D2 E2^F2  | GAG^F G2A2 B2GB  | B | c4 C2D2 E2^F2  | GAG^F G4 G,4  :|: efed e2c2 c2de  | fefe d2c2 B2G2  | f2ed e2c2 c2de  | fefd e2c2 d2ef  | g2eg gfdf fece  | edcB c4 C4  :|: G2AB c2d2 e2c2  | B2A2 d2c2 B2A2  | G2AB c2de fedc  | B2c2 d4 D4  :| efed e2c2 c2de  | fefe d2c2 B2G2  | f2ed e2c2 c2de  | fefd e2c2 d2ef  | g2eg gfdf fece  | fdcB c4 C4 |]"
+    abc: "%%abc-charset utf-8\nX:1000\nT:Nuorten miesten speli\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0001\nK:C\n|: B | c4 C2D2 E2^F2  | GAG^F G2A2 B2GB  | B | c4 C2D2 E2^F2  | GAG^F G4 G,4  :|: efed e2c2 c2de  | fefe d2c2 B2G2  | f2ed e2c2 c2de  | fefd e2c2 d2ef  | g2eg gfdf fece  | edcB c4 C4  :|: G2AB c2d2 e2c2  | B2A2 d2c2 B2A2  | G2AB c2de fedc  | B2c2 d4 D4  :| efed e2c2 c2de  | fefe d2c2 B2G2  | f2ed e2c2 c2de  | fefd e2c2 d2ef  | g2eg gfdf fece  | fdcB c4 C4 |]"
   },
   {
     name: "kt1_0002",
-    abc: "%%abc-charset utf-8\nX:1001\nT:kt1_0002\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0002\nK:G\n|: G2A[BB] c4 c4  | e | d2c2 cB4 B4  | G2AB c4 d2e2  | e2dc c2d2 d4  | edef g2f2 d2f2  | e2dc B4 c4  | edef g2e2 d2c2  | edcB c4 C4  :|: edef g4 g2f2  | gfed e2dc d4  | g4 a2g2 f2e2  | e2dc c2d2 d4  | edef g2e2 d2f2  | e2dc B4 c4  | edeg f2e2 d2c2  | edcB c4 C4  :| |]"
+    abc: "%%abc-charset utf-8\nX:1001\nT:kt1_0002\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0002\nK:C\nG2A[BB] c4 c4  | e | d2c2 cB4 B4  | G2AB c4 d2e2  | e2dc c2d2 d4  | edef g2f2 d2f2  | e2dc B4 c4  | edef g2e2 d2c2  | edcB c4 C4  | G2A[BB] c4 c4  | e | d2c2 cB4 B4  | G2AB c4 d2e2  | e2dc c2d2 d4  | edef g2f2 d2f2  | e2dc B4 c4  | edef g2e2 d2c2  |: edcB c4 C4  | edef g4 g2f2  | gfed e2dc d4  | g4 a2g2 f2e2  | e2dc c2d2 d4  | edef g2e2 d2f2  | e2dc B4 c4  | edeg f2e2 d2c2  :| edcB c4 C4 |]"
   },
   {
     name: "kt1_0003",
-    abc: "%%abc-charset utf-8\nX:1002\nT:kt1_0003\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0003\nK:G\n|: cdef g2g2 ef2f2 | e2fg agfe f4  | e2ef d2de c2c2  | B2BG AGAB G4  | c2c2 dcBG c4  | d2e2 fefd g4  | agfe f2ed e2c2  | edcB c4 C4  :|: e2c2 c2e2 e2g2  | dcBA GABc d2e2  | c2cB A2Ac B2G2  | BAG^F G2D2 B,2G,2  | G2GF ECEF G2G2  | A2A2 B2BG c4  | G2GF ECEF G2G2  | A2A2 B2BG c4  | c2de d2cB c4  | d2e2 f2ed g4  | agfe f2ed e2c2  | edcB c4 C4  :| |]"
+    abc: "%%abc-charset utf-8\nX:1002\nT:kt1_0003\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0003\nK:C\n|: cdef g2g2 ef2f2 | e2fg agfe f4  | e2ef d2de c2c2  | B2BG AGAB G4  | c2c2 dcBG c4  | d2e2 fefd g4  | agfe f2ed e2c2  | edcB c4 C4  :| e2c2 c2e2 e2g2  | dcBA GABc d2e2  | c2cB A2Ac B2G2  | BAG^F G2D2 B,2G,2  | G2GF ECEF G2G2  | A2A2 B2BG c4  | G2GF ECEF G2G2  | A2A2 B2BG c4  | c2de d2cB c4  | d2e2 f2ed g4  | agfe f2ed e2c2  | edcB c4 C4  | e2c2 c2e2 e2g2  | dcBA GABc d2e2  | c2cB A2Ac B2G2  | BAG^F G2D2 B,2G,2  | G2GF ECEF G2G2  | A2A2 B2BG c4  | G2GF ECEF G2G2  | A2A2 B2BG c4  | c2de d2cB c4  | d2e2 f2ed g4  | agfe f2ed e2c2  | edcB c4 C4 |]"
   },
   {
     name: "Morsianspeli pijkains kansa",
-    abc: "%%abc-charset utf-8\nX:1003\nT:Morsianspeli pijkains kansa\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0004\nK:G\n|: c2de f2f2 e2e2  | d2c2 B2A2 B2G2  | g2fg a2a2 g2g2  | fedf e2c2 c4  :|: g2fg f2df e2c2  | f2d2 efe[ce] d4  | c2B2 c2de fedc  | edcB c4 C4  :| |]"
+    abc: "%%abc-charset utf-8\nX:1003\nT:Morsianspeli pijkains kansa\nS:Samuel Rinda-Nickola\nM:3/4\nL:1/16\nR:Nuotti-kirja\nB:eSävelmät kt1_0004\nK:C\n|: c2de f2f2 e2e2  | d2c2 B2A2 B2G2  | g2fg a2a2 g2g2  | fedf e2c2 c4  :|: g2fg f2df e2c2  | f2d2 efe[ce] d4  | c2B2 c2de fedc  | edcB c4 C4  :| |]"
   }
 ];
