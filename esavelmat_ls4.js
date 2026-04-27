763e5934e9103e8aed5a6198a9614729cf28de5f
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-27 18:40:01 +0200

    Add files via upload

--- esavelmat_ls4.js
+++ esavelmat_ls4.js
@@ -1,10 +1,10 @@
 var esavelmat_ls4 = [
   {
-    name: "ls4_2379",
-    abc: "X:1\nT:ls4_2379\nO:Kiukainen, Satakunta\nS:K. Pastell\nM:2/4\nL:1/16\nR:\nN:AIVd4\nZ:\nB:eSävelmät ls3_2379\nK:a\n|: A4 e4 | e4 e4 | e4 d2d2 | d4 B4 | c4 c2B2 | c4 d4 | e8 | B8 | c4 A4 | A4 A4 | A4 ^G4 | ^G4 B4 | e4 e4 | ^G4 ^G4 | A8 | A8:|"
+    name: "ls4_3334",
+    abc: "X:1\nT:ls4_3334\nO:Sulkava, Inkeri\nS:E. Sivori\nM:2/4\nL:1/16\nR:\nN:C III b 6\nZ:\nB:eSävelmät ls4_3334\nK:d\n|: A2A2 A2A2 | f4 e4 | d4 d4 | F4 A4 | c4 c2c2 | e4 d4 | A8 | A4 F2A4 | A2G2 F4 | A4 c6 | B2G4 G4 | F4 F4 | G4 G4 | A8 | A4:|"
   },
   {
-    name: "ls4_2380",
-    abc: "X:1\nT:ls4_2380\nO:Rautalampi, Pohjois-Savo\nS:I. Krohn\nM:2/4\nL:1/16\nR:\nN:AIVd4\nZ:\nB:eSävelmät ls3_2380\nK:a\n|: c4 ^G4 | A4 B4 | c4 ^G4 | A4 B4 | e4 B2B2 | c4 d4 | e8 | B8 | B4 e4 | e4 d4 | e4 ^G4 | A4 B4 | c4 A2A2 | ^G4 ^G4 | A8 | A8:|"
+    name: "ls4_3335",
+    abc: "X:1\nT:ls4_3335\nO:Rautalampi, Pohjois-Savo\nS:I. Krohn\nM:3/4 ja 2/4\nL:1/16\nR:\nN:C III b 7\nZ:\nB:eSävelmät ls4_3335\nK:C\n|: G3G E4 c4 | G3F E4 D4 | c3c B4 d4 | A4 G8:| | G4 e4 d4 | c2B2 A4 B4 | c4 A4 c3c | B4 d4 A4 | G8 G4 | e4 d4 c2B2 | A4 B4 c4 | A4 c3c B4 | d4 A4 G8:|"
   }
 ];
