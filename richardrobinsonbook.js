c97e3e5c42c4fc41385372555cc6f3f11d6f65eb
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-27 10:37:03 +0200

    Add files via upload

--- richardrobinsonbook.js
+++ richardrobinsonbook.js
@@ -17633,7 +17633,7 @@ var richardrobinsonbook = [
   },
   {
     "name": "Klezmeron",
-    "abc": "X:1\nT:Klezmeron\nL:1/16\nM:4/4\nK:D minor\nA,2 D2F2|:A6 A4 BA G2F2|A2 A6 z2 A,2 D2F2|A6 A4 BA G2F2|A8 z2 A2 d2e2| f8 z2 d2 B2G2 | B8 z2 E2 F2G2|\nABA2 GAG2 FGF2 EFG2| A8 z2 A2 d2e2|f8 z2 d2 B2G2| B8 z2 E2 F2G2|ABA2 GAG2 FGF2 EFE2|1 D8 z2 A,2 D2F2:|2 z8 z2 A,2B,2A,2|\nL:1/8\n|:A,2 B,2 ^C2 D2| E E3 z A,^CE|G G2 _A =A>G FG|A>B A>B A A, B,A,|\nA,2 B,2 ^C2 D2| E E3 z A,^CE|G G2 _A =A>G FE|1 D4 z A, B,A,:|2 D8|\nc2 A>B c4| B2 A>G FA C2|c2 A>B c4|B2 A>G F4|\nc2 A>B c3 c|f/2e/2d c/2B/2A AB =Bc|c2 A>B c4|B2 A>G F4|\nB3 B2 E FG| A4 z D EF|A2G2F2G2|\nL:1/16\nA8 z2 A2 d2e2|f8 z2 d2 B2G2 | B8 z2 E2 F2G2|ABA2 GAG2 FGF2 EFE2| D8 z2 A,2 D2F2|"
+    "abc": "X:1\nT:Klezmeron\nC:Richard Robinson Tunebook\nL:1/16\nM:4/4\nK:D minor\nA,2 D2F2|:A6 A4 BA G2F2|A2 A6 z2 A,2 D2F2|A6 A4 BA G2F2|A8 z2 A2 d2e2| f8 z2 d2 B2G2 | B8 z2 E2 F2G2|\nABA2 GAG2 FGF2 EFG2| A8 z2 A2 d2e2|f8 z2 d2 B2G2| B8 z2 E2 F2G2|ABA2 GAG2 FGF2 EFE2|1 D8 z2 A,2 D2F2:|2 z8 z2 A,2B,2A,2|\nL:1/8\n|:A,2 B,2 ^C2 D2| E E3 z A,^CE|G G2 _A =A>G FG|A>B A>B A A, B,A,|\nA,2 B,2 ^C2 D2| E E3 z A,^CE|G G2 _A =A>G FE|1 D4 z A, B,A,:|2 D8|\nc2 A>B c4| B2 A>G FA C2|c2 A>B c4|B2 A>G F4|\nc2 A>B c3 c|f/2e/2d c/2B/2A AB =Bc|c2 A>B c4|B2 A>G F4|\nB3 B2 E FG| A4 z D EF|A2G2F2G2|\nL:1/16\nA8 z2 A2 d2e2|f8 z2 d2 B2G2 | B8 z2 E2 F2G2|ABA2 GAG2 FGF2 EFE2| D8 z2 A,2 D2F2|"
   },
   {
     "name": "Tico  Tico",
