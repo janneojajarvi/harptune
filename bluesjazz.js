079b0b131bbfbb66298efab0faa146f6c1576d66
janneojajarvi <janne.ojajarvi@gmail.com>
2026-06-19 00:25:40 +0300

    Add files via upload

---
+++ bluesjazz.js
@@ -0,0 +1,154 @@
+var bluesjazz = [
+  {
+    name: "All Of Me",
+    abc: "X:1\nT: All Of Me\nC: Seymour Simons & Gerald Marks\nM: 4/4\nK: Cmaj\nL: 1/4\n c> G E2- | E2 (3 c d c |  B> ^G E2- | E4 |\nw: All of me, | * why not take | all of me.*\n A> G E2- | E ^D (3 E _B A |  G2 F2- | F4 ||\nw: Can't you see | I'm no good with-| out you?*\n E> _E D2- | D2 (3 E ^G B |  d2 c2- | c4 |\nw: Take my lips | * I want to | lose them.*\n B>_B A2- | A2 (3 A D B |  A4 |  G4 ||\nw: Take my arms | * I'll ne-ver | lose | them.\n c> G E2- | E2 (3 c d c |  B> ^G E2 |\nw: Your good-bye | * left me with | eyes that cry\n A> G E2- | E ^D (3 D _B A |  G2 F2 ||\nw: How can I | * go on dear with- | out you?\n c4 c B |  d3 c |  B2 E G |  B3 A |\nw: You took the | part that | once was my | hear, so\n c2 A c |  e2 e2 |  c4 | z2  z2 |]\nw: Why not take | all of | me"
+  },
+  {
+    name: "Autumn Leaves",
+    abc: "X:1\nT: Autumn Leaves\nC: Joseph Kosma\nL: 1/8\nM: 4/4\nK: G\nz2 E2 F2 G2 |: c8 | z2 D2 E2 F2 | B4 B4 | z2 C2 D2 E2 |\nA8 |1 z2 B,2 ^C2 ^D2 | G8 | z2 E2 F2 G2 :|2 A2 F2 A2 G2 |E8 | z4 ^D2 E2 |\nF2 B,2 F4 | z2 F2 E2 F2 | G8 | z2 G2 F2 G2 |\nA8 | z2 D2 d2 c2 | B8 | z4 ^A2 B2 |\nc2 c2 A2 A2 | F6 c2 | B4 B4 | z6 E2 |\nA6 G2 | F4 G2 B,2 | E8 |"
+  },
+  {
+    name: "Bye Bye Blackbird",
+    abc: "X:1\nT:Bye Bye Blackbird \nL:1/4\nM:4/4\nK:F\nAA AA|BA A2|AG G2|GF F2|\nF4|G4|F2 E2-|E4||\nBBBB|cB B2|BAA2|AGG2|\nG4|A4|G2F2-|F2-F2||\nFccc|cBAG|G4|^F4|\nDBBB|BAGF|F4|E4|\nAA AA|BA A2|AGG2|G^FF2|\nG2B2-|B2 E2|F4|z2 z2|]"
+  },
+  {
+    name: "C'est Si Bon",
+    abc: "X:1\nT:C'est Si Bon \nC:Henri Betti, 1947\nM:4/4\nL:1/8\nK:Cmaj\nz2G2 ^F2|=F2z2-z2-z2| z2 (GA B>A G2)| E2z2 z2z2|z2 (GA B>A  G2)|\n [FD]2z2-z2z2| z(G A>B) e2 B2 |A3E BcEF| G2 zE GG ^FF|\n=F8| z G A>B A2 G2| E8|z2 GA B>A  G2|\n F8-| zG A>B e2 d2 |c8-|c2 z2 c2 _d2|\n|| _e4 c2 _B2|_A4 G2 F2| F4 _E4-|_E2 z2 c2 ^c2|\n d4 =c2 B2|A4 B2 c2| [cA=Fe]6[c_e]2|d2 z2 G2 ^F2||\n=F2z2-z2-z2| z2 GA B>A G2| E2z2 z2z2|z2 GA B>A  G2|\n=F8-| z GBd g2 g2| [G_Bg]8|[ge^cA]2 z2 [ged_B]2 [_g_e_dB]2|\n[fd=cA]8|(3z2e2d2 (3f2e2d2|[ecA]8| z2ed cB A2|\n _A4 z=Ac_e |dAGA [e^cF]2[e^cF]2|c4- c4-|c2z2z4|]"
+  },
+  {
+    name: "Christopher Columbus",
+    abc: "X:1\nT:Christopher Columbus\nN: D/G melodeon version\nM:4/4\nL:1/8\nK:Gmaj\n|G2 z2 B2 z2|c2 B2 c2 ^cd|zg -g z  z4| AB=c^c  d=cBA|\n|G2 z2 B2 z2| c2 B2 c2 ^cd| z G-G z z4| z4-z2- z2||\n|G2 z2 B2 z2|c2 B2 c2 ^cd|zg-g z z4| AB=c^c  d=cBA|\n|G2 z2 B2 z2| c2 B2 c2 ^cd| z G-G z z4| z8 ||\n| z2 f2 f3 f|g a3 f4| e2 =f2 ^f2 =fe-|e6 z2|\n| z2 e2 e3 e|f g3 e4| d2 _e2 =e2 _ed|z2 c2 B2 A2|\n|G2 z2 B2 z2|c2 B2 c2 ^cd|zg-g z z4| AB=c^c  d=cBA|\n|G2 z2 B2 z2| c2 B2 c2 ^cd| z G-G z z4| z8||\n|G2 z2 B2 z2|c2 B2 c2 ^cd|zg-g z z4| AB=c^c  d=cBA|\n|G2 z2 B2 z2| c2 B2 c2 ^cd| z G-G z z4| z||"
+  },
+  {
+    name: "The days of wine and roses",
+    abc: "X:1\nT:The days of wine and roses\nM:4/4\nL:1/4\nC:Henry Mancini & Johnny Mercer\nK:F\nV:chords\nx || x2 x2 | x2 x2 | x2 x2 | x2 x2 |\nx2 x2 | x2 x2 | x2 x2 | x2 x2 ||\nV:melody\nC||A4-|AGAc|d2c2-|c2cA|\nADD2-|D2cA|A_DD2-|D2AG||\nV:chords\nx2 x2 | x2 x2 | x2 x2 | x2 x2 |\nx2 x2 | x2 x2 | x2 x2 | x2 x2 ||\nV:melody\nGCD>E|GFAd|c3B|cBAG|\nA3E|GFE>D|G4-|G3C||\nV:chords\nx2 x2 | x2 x2 | x2 x2 | x2 x2 |\nx2 x2 | x2 x2 | x2 x2 | x2 x2 ||\nV:melody\nA4-|AGAc|d2c2-|c2cA|\nADD2-|D2cA|A_DD2-|D2AG||\nV:chords\nx2 x2 | x2 x2 | x2 x2 | x2 x2 |\nx2 x2 | x2 x2 | x2 x2 | x2 x2 ||\nV:melody\nGCDE|FGAc|e4-|e3d|\ncAGF|AA2D|F4-|F2z2|]"
+  },
+  {
+    name: "Don't Get Around Much Anymore",
+    abc: "X:1\nT:Don't Get Around Much Anymore \nC:Duke Ellington\nM:4/4\nL:1/8\nK:C\nze2d cGFE-|E4-E-E-E2|ze2d cGFE|E4-E2-E2|\nzG2F EDCc-|c3 A-A4|zEF^F GC^DE|C8:|C8|\nd4c A2c-|c6 c2|d4c G2E-|E6 c2|\nd4c A2c-|c6 z2|zBBB BA2G-|Ge2d cGFE-|\nE4-E-E-E2|ze2d cGFE|E4-E2-E2|zG2F EDCc-|\nc3 A-A4|zEF^F GC^DE|C8|z4 z4|]"
+  },
+  {
+    name: "Embraceable You - with bass line (Eb)",
+    abc: "X:1\nT:Embraceable You  - with bass line (Eb)\nL:1/4\nM:4/4\nK:Ebmaj\nV:1 scale=1.2 \nzCDE|zCDE|F>B, B,B,-|B,4|\nzFGA|zFGA|B>E EE-|E4|\nGEFE|GEFE|G>D D D-|D4|\nFDED|EFG=A|B>B,B,B,-|B,4|]\nzCDE|zCDE|F>B, B,B,-|B,4|\nzFGA|zFGA|B>E EE-|E4|]\nzBAG|AGFG|E E/2E/2 E/2E/2 E|=A3z|\nzGAB|=Bc2D|E4|z4|]\nV:2 clef=bass scale=1.2 \nG,2zz|_G,2zz|F,2_D,2|D,2(3A,G,^F,|\nF,2zE,|D,2B,B,|E,2F,2|_G,2=G,2|\nC,2CB,|=A,zC,<_A,|G,2_G,2|F,2=E,D,|\nB,,z=B,,z|E,D,_D,C,|B,,2B,D,|_D,zB,=D,|]\nE,zE,z|_G,zG,z|F,z_D,z|=D,2z^F,|\nF,2zE,|_D,2B,_D|E2Cz|B,B,EE,|]\nA,zA,z|D,zG,z|C,C_CB,|=A,z=A,,_A,,|\nB,,zC,B,,|_A,,zB,,B,|E,E,B,,B,,|F,2F,,2|]"
+  },
+  {
+    name: "Emily",
+    abc: "X:1\nT:Emily \nC:Johnny Mercer & Johnny Mandel\nL:1/8\nM:3/4\nK:C\nEB G4|EB G4|EB G4-|G2 E3D|\nC3D E2|A4 G2|C6-|C4 D2|\nEA, A,4|EA, A,4|EA, A,4-|A,2 A,2 B,2|\nC2 D2 E2|d2 c2 A2|D6|^D6|\nEB G4|EB G4|EB G4-|G2 E3D|\nC3D E2|e3d c2|A6|B6|\nc2 B2 A2|G4 ^F2|GA B4-|B2 A2 G2|\nFG A4-|A2 G3F|EF G4-|G2 A2 B2|\nc2 B2 A2|G4 F2|EB G4-|G2 G2 F2|\nEB G4|EB G4|C6-|C4 z2|]"
+  },
+  {
+    name: "Georgia on my Mind",
+    abc: "X:1\nT:Georgia on my Mind\nL:1/4\nM:4/4\nK:F\nA c3|A G3|z A d A|G3F/G/|\nAc ed|Bd AG|c2-c2|d/d/d/d/ ^G2|\nA c3|A G3|z A d A|G3F/G/|\nAc ed|BD AA|F2-F2-|FF z2|\nDF GA|FD F2|DF GA|cA =B2|\nDF GA|cde>d|cA cA|A2 ^G2|\nA c3|A G3|z A d A|G3F/G/|\nAc ed|BD AA|F4-|F2 z2|]"
+  },
+  {
+    name: "Honeysuckle Rose",
+    abc: "X:1\nT:Honeysuckle Rose \nC: Fats Waller & Andy Razaf, 1929\nM:4/4\nL:1/8\nK:F\ncB DF A4|cB DF A4|cB DF A2A2|A4 AG FD|\nF2F2 F4-|F4 _AG FD|F4-F4|z4 z4|\ncB DF A4|cB DF A4|cB DF A2A2|A4 AG FD|\nF2F2 F4-|F4 _AG FD|F4-F4|z4 z4|]\nF2F2 G2G2|^G2G2 A2A2|z2Bc- cBc2|_d2cB-B4|\nG4 A4|B4 =B4|z2cd- dcd2|_e2dc- c4|\ncB DF A4|cB DF A4|cB DF A2A2|A4 AG FD|\nF2F2 F4-|F4 _AG FD|F4-F4|z4 z4|]"
+  },
+  {
+    name: "Hot Toddy",
+    abc: "X:1\nT:Hot Toddy \nL:1/8\nM:4/4\nK:F\n|z2c2 Bc B2|c2c2 BcB2|c2cA-A4|c2cA-A4|\nz2c2 Bc B2|c2c2 BcB2|c2cA-A4|c2cA-A4|\nz2f2_efe2|f2f2_efe2|f2fd-d4|f2fd-d4|\nz2g2fgf2|g2g2fgf2|g2_ed-d4|d6z2|\n|z2c2 Bc B2|c2c2 BcB2|c2cA-A4|c2cA-A4|\nz2c2 Bc B2|c2c2 BcB2|c2cA-A4|c2cA-A4|\nz2f2_efe2|f2f2_efe2|f2fd-d4|f2f_d-d4|\nz2c2 Bc B2|c2c2 BcB2|c2c2 BcAF-|F6 z2|]"
+  },
+  {
+    name: "I've Got the World On a String",
+    abc: "X:1\nT:I've Got the World On a String \nL:1/8\nM:4/4\nK:Ebmaj\nB, EG|:cB_dc-c4|BAeA G F3|FEBE D E2 D-|D4 _D4|\nC2C2 B,2CC-|C B,3 G2 G2|1G4-G4|z4 zB,EG:|2 E8|zGGG GGG=A-|]\n=A8|zGGG GGG=A-|A8|zGGG GG=Ad|\n=A8|zFFF FFFG-|G8-|G4 zB,EG|\ncB_dc-c4|BAeA G F3|FEDE GB2d-|d4_d4|\ne2e2 c2ee-|e c3 B2G2|E4-E4-|E4-E4|]"
+  },
+  {
+    name: "Just Friends",
+    abc: "X:1\nT:Just Friends\nL:1/4\nM:4/4\nK:G\nB|:B4-|B2(3ABA|_E4-|E2 A2|\nA4-|A A (3GAG|_D4-|D3G|\nG>EG>E|F3 F|F>DF>D|E F G B|\ne2 B2-|B E (3EFG|B2 A2-|A2 B2|\nB4-|B2(3ABA|_E4-|E2 A2|\nA4-|A A (3GAG|_D4-|D3G|\nG>EG>E|F3 F|F>DF>D|G A B d|\ne3E|d2 B2|G4|z2 B2 :|"
+  },
+  {
+    name: "La Mer (Beyond the Sea)",
+    abc: "X:1\nT:La Mer  (Beyond the Sea)\nC:C. Trenet\nL:1/8\nM:4/4\nK:F\n|:C2|F8-| F4 (3E2F2G2|A6F2|G4 (3E2D2C2 | \nA8- | A4 (3G2A2B2 | c3FF3F | d8- | \nd2d2 (3c2A2G2 | F4D2F2 |1G8|C6:|2G8 |F4z2E2 |]\n|A8-|A4 (3^G2A2=B2 | ^c6A2 | =B4 (3^G2^F2E2 |\nE8-|E4z2=G2|c8- |c4 (3=B2c2d2 | \ne6c2 | d3=B (3B2A2G2 | c8-|c2z2z2C2| \nF8-| F4 (3E2F2G2 | A6F2 | G4 (3E2D2C2 | \nA8- | A4 (3G2A2B2 |c3FF3F | d8- | \nd2d2 (3c2A2G2 | F4D2F2 | G8 | F6z2 |]"
+  },
+  {
+    name: "Lullaby of Birdland",
+    abc: "X:1\nT:Lullaby of Birdland\nC: George Shearing\nM:4/4\nL:1/8\nK:Gm\n|d2 cB A G2 =E-|E2 G^F-F2 z D-|D2 AG-G2  z E-|E2 dc-c2 z f-|\nf2 ed c B2 G-|Gdc^F- Fdc=F-|1F4 z c BE-|EBAD-D4:|2E4 z F AB|z8|]\n|d2 e=e f2 f_e|z c3-c2 z2|c2 ^cd e2 ed-|d4 z4|\n|d2 e=e f2 f_e|z c3-c2 z2|c2 ^cd e2 ed-|dedc  z4||\n|d2 cB A G2 =E-|E2 G^F-F2 z D-|D2 AG-G2 z E-|E2 dc-c2 z f-|\n|f2 ed cB2 G-|Gdc^F- Fdc=F|F4 zF AB|z8|]"
+  },
+  {
+    name: "Midnight in Moscow",
+    abc: "X:1\nT:Midnight in Moscow   \nL:1/4\nM:4/4\nK:Dm\nD F A F|G2 FE|A2 G2|D2 z2|\nF A c c|d2 cB|A4|=B2 ^c2|\nedA2|zFED|AG B2-|B2 cB|\nA2 GF|AGFE|D2 A2|=B2 ^c2|\ned A2|z FED|AG B2-|B2 cB|\nA2 GF|A2 G2|D4|z4|]"
+  },
+  {
+    name: "Moon River",
+    abc: "X:1\nT:Moon River \nL:1/4\nM:3/4\nK:C\n|G3|d c2|B>A G/F/|G2C|\nB>A G/F/|G2C|D3-|D2E|\n|1 C3|GE>D|C3|GE>D|CEG|cB>A|BA>G|A2-A:|\n|2 C3|E2G|c3|d2c|G3-|G B/A/ G/F/|G3-|G/C/ B/A/ G/F/|\nG3|C3|FD2-|D2E|C3-|C3|]"
+  },
+  {
+    name: "My Melancholy Baby",
+    abc: "X:1\nT:My Melancholy Baby \nL:1/4\nM:4/4\nK:C\nEF^FG|dcG^F|F4|E4|\nED^CD|F2B2|A4-|A4|\nDEFG|AcBA|E4|D4|\nDCB,C|DCDE|G4-|G4|\nEF^FG|dcG^F|F4|E4|\nED^CD|F2B2|A4-|A4|\nA^GAB|c2BA|G^FG^F|A4|\nFG^GA|EGED|C2 z2|z2 z2|]"
+  },
+  {
+    name: "Satin Doll",
+    abc: "X:1\nT:Satin Doll  \nC:Billy Strayhorn, Duke Ellington & Johnny Mercer\nM:4/4\nL:1/8\nK:C\n AGAG-  GA3 |  zA3  GA3 |  BABA-  AB3 |  zB3  AB3 |\n zd3  cd3 |  z_B3  _A2BG- |  G8- |  G4-G4 |]\n AGAG-  GA3 |  zA3  GA3 |  BABA-  AB3 |  zB3  AB3 |\n zd3  cd3 |  z_B3  _A2BG- |  G4-G4 | z4 z3G |]\n c2BA GAB2 |  c2BA GABc- |  c8- | c6 zc |\n dcBA- ABc2 |  dcBA- ABcd- |  d8 | z8 |]\n AGAG-  GA3 | zA3 GA3 |  BABA-  AB3 |  zB3  A B3 |\n zd3  cd3 |  z_B3  _A2BG- |  G8- | G2z6 |]"
+  },
+  {
+    name: "Shiny Stockings",
+    abc: "X:1\nT:Shiny Stockings\nL:1/8\nM:4/4\nK:Fmaj \n^G2|:A2z2GA2c|zA3G2A2|D2G2AG2A|z4z^F3|\nG2z2FG2c|_zG3F2G2|C2F2GF2G|z4z^F3|\nF2z2DFAc-|c4-c _A3|G2z2EG=Bd-|1d4-dz _BA|\n=z4^FA^ce-|e4-e ^c3|A2z2z2z_e|z_ed2cB3:|2\nd4-d e3|f2d2cB2A|zF3AA2F|z8|z4z4|]"
+  },
+  {
+    name: "Smooth One",
+    abc: "X:1\nT:Smooth One\nL:1/8\nM:4/4\nK:F\n|:d_A3 FGFD|FA3 z4|d_A3 FGFD|FD3 z4|\nd_A3 FGFD|FA3 z2(3D^C=C|=B,DFA DD2-D-|1D6z2:|2D4 z2 FG|]\n|G2FD-D2 FG-|G2 F_A z2 Bc-|c2 BA-A2 FG-|G6 FG-|\nG2 FD-D2 FG|G2 F_A z4|Acdc _ecdB|c2 BA-AF-F2|\nd_A3 FGFD|FA3 z4|d_A3 FGFD|FD3 z4|\nd_A3 FGFD|FA3 z2(3D^C=C|=B,DFA DD2-D-|D6z2|]"
+  },
+  {
+    name: "Stella By Starlight (with bass line)",
+    abc: "X:1\nT:Stella By Starlight (with bass line)\nL:1/4\nM:4/4\nK:Bb \nV:1 scale=1.2\nB||A4-|AGAB|F4-|F2F2|\nG4-|GFFG|B,4-|B,2C2|\nEDCB,|D3=E|G>FF2-|F3G|\nBAGF|G2-(3GAB|d>cc2-|c2d2||\ne4-|eeed|f4-|fe(3dcB|\nd2d2-|dcBA|c4-|c2B2|\nA4-|AGAB|F4-|FFFE|\n_G4-|G_GGF|F4-|F4|]\nV:2 clef=bass scale=1.2\nz||=E,D,C,B,,|A,, ^C,E,G,|C,G,E,C,|F,,A,,C,F,|\n_A,G,F,E,|D,_CB,_A,|G,B,E,D,|C,E,_A,,_G,,|\nF,,A,,B,,D,|=E,G, A,G,|F,A,F,D,|_D,F, E,G,|\nF,CA,F,|B,=E, A,A,,|C,D,E,G,|^F,A,D,C,||\n=B,,E,G,B,|=B,DEG,|CDEF|G_GF=E|\nEDCB,|_A,_G,F,E,|D,F,A,C|B,A,G,F,|\n=E,D,B,,G,,|A,,B,A,G,|F,E,D,C,|=B,,_A,G,F,|\nE,_D,C,B,,|A,,_G,F,E,|D,F,B,A,|B,F,D,F,|]"
+  },
+  {
+    name: "Tequila",
+    abc: "X:1\nT:Tequila\nL:1/8\nM:4/4\nK:Dmaj\n|:.d2.d2 =c2zd-|d.d2d =cc c2|.d2.d2 =c2zd-|d=c2c cccA:|\n|:.d2d=c e2cd-|d A2z z2z A|.d2d=c ec2d-|d2z2z2zA|\nd2d=c e c2d-|dA2z z2zA|d2d=c ec2d-|d2z2zABd|\n=f4-f3d-|d2z2zABd|=f4-f3d-|d2z2zABd|\n=f4-f3d-|d2z2z4|eeee e2 e2|.A2z2z4:|"
+  },
+  {
+    name: "Things Ain't What They Use to Be",
+    abc: "X:1\nT:Things Ain't What They Use to Be \nC:Duke Ellington\nM:4/4\nL:1/8\nK:F\n(3zCF (3Acd c2dF-|F3 A z4|(3zCF (3Acd c2dF-|F3 A z4|\n(3zFB (3dfg f2gB-|B3_d z dcA|(3zCF (3Acd c2dF-|F3 A z2zc|\nf_e c4 zc|(3f_ec (3f_ec e2 =ef-|f2z2 z4|z4 z4:|"
+  },
+  {
+    name: "This Guy's In Love With You",
+    abc: "X:1\nT: This Guy's In Love With You\nC: Herb Alpert & Hal David\nM:4/4\nL:1/8\nK:Eb\n|:z2 GG-G2 G{/F}G-|G3 z z2 z G|c2 C2 E F2 E/2F/2-|F4 z4|\n z2 G{/F}G-G2 G{/F}G-|G3 z z2 z G|G2 G2 =A2 =B2|c e2 e-e4|\n_d e2 e-e4|_d2 f2 e2 d2|e2 f2 g f3|FG- GG- G2 z F|\nB4 z2 z G|cE-E2 z2 z F|B6 G2|c2 E2 FE F2:|\nz2 G{/F}G-G2 G{/F}G-|G4 z4|z2 G{/F}G-G2 G{/F}G-|G4 z4|\nz2 GG-G2 =A2|B6 =A2|G4 c =A3|F4-F2-F2|\\nF2 F2 (3A2G2F2|G/2F/2E-E6|"
+  },
+  {
+    name: "Undecided",
+    abc: "X:1\nT:Undecided \nL:1/8\nM:4/4\nK:C\nB2|:BcBc A3c|Bc A4-Ac|BcBc A3c|B2 cA-A2z A|\nBcBc A3c|dd c2 _e2 dc-|1 c4-c4|z4z4:|2c8-|c4z2Bc|]\n|ddc_B c2 Bc|ddc_B c2 Bc|ddcA c2 Ac|dc3-c2 Ac|\nddcA c2Ac|ddcA c2Ac|ddBGB2 GB|dG-G4z2|"
+  },
+  {
+    name: "What A Wonderful World",
+    abc: "X:1\nT: What A Wonderful World \nK:F\nL:1/4\nM:4/4\nC/2E/2||: F> F  c2| z/2 d/2 d/d/2  c2| z/2 B/2 B/2B/2  A2| z/2 G/2 G/2G/2  F2|\n z/2 F/2 F/2F/2 F2-| F2  z F/2F/2| E/2F/2 G  A2-| A2  z C/2E/2|\n F > F  c2| z/2 d/2 d/2d/2  c2| z/2 B/2 B/2B/2  A2| z/2 G/2 G/2G/2  F2|\n z/2 F/ F/2F/2 F2-| F2  z F/2F/2| E/2F/2 G  F2-| F2  z F/2F/2|]\nG/2 G G/2G/2 G/2 C| B/2 A G/2 A > F| G/2 G G/2G/2 G/2 C| B/2 A G/2 A A/2c/2|\n d-(3d/2d/2d/2  c-(3c/2A/2c/2| d-(3d/2d/2d/2  c2|  d > d  c > A| B  A  G  C/2E/2|\n F > F  c2| z/2 d/2 d/2d/2  c2| z/2 B/2 B/2B/2  A2| z/2 G/2 G/2G/2  F2|\n z/2 F/2F/2 F2-| F2  z F/2F/2| E/2F/2 G  F2-| F2  z  C/2E/2:|"
+  },
+  {
+    name: "Work Song",
+    abc: "X:1\nT: Work Song\nK: Em\nL: 1/4\n|z E/2E/2 G/2 A B/2-|B B/2A/2 B d-|d E/2E/2 G/2 A E/2-|E E/2D/2 (3E/2D/2B,/2-B,-|\nB, E/2E/2 G/2 A B/2-|B B/2A/2 B d-|d B/2B/2 d/2 B ^d/2-|^d3 z|\ne/2d/2 B/2A/2G/2E/2|G G/2E/2 G A-|A E/2E/2 G/2 A E/2-|E E/2D/2 E E/2E/2|\n_B/2 3B/2-B E/2E/2|_B/2 3A/2-A/2E/2G/2_B/2-|_B/2 A G/2  A/2G/2 (3E/2D/2E/2-|E3 z|]"
+  },
+  {
+    name: "Saint Louis Blues",
+    abc: "X:1\nT:Saint Louis Blues\n% last edit 03-02-2013\nC:W.C.Handy\nM:2/4\nL:1/16\nQ:1/4=72\nK:G\n  z2|{^A}BdBG- G4 |z ^DEG _B A3|G8-  |G6 \\n  z2|    zEGA- A4 |z  EGA _B A3|G8-  |G6\n  z2|    FAFD  D4 |z2 _BB  B A3|G8-  |G6 \\n  z2|{^A}BdBG- GG3|z ^DEG _B A3|G8-  |G6\n  z2|    zEGA- AA3|z ^DEG _B A3|G8-  |G6 \\n  z2|    FAFD- DD3|z2 _BB  B A3|G8-  |G4||\n%\nzddd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4- A  \\n c2c|    cc3-  c4 |z2 c^c  d A3|G8-  |G4  z2\n  dd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4  z2 \\n  c2|    cc2c- c4 |z2 c^c  d A3|G4 A4|d6||\n%\n(^AB)| G2(^AB) G2(AB) |G2(^AB) (GA)B  G-|G8-|G4 z2\n(^AB)|=A2  GE  G2(A_B)|A2(GE)   G(^AB)G-|G8-|G4 z2\n(^AB)| d2(^AB) D(AB)C-|C(^AB)D- D (AB)G-|G8 |z4 z2|]"
+  },
+  {
+    name: "St Louis Blues",
+    abc: "X:1\nT:St Louis Blues\n% last edit 03-02-2013\nC:W.C.Handy\nM:2/4\nL:1/16\nQ:1/4=72\nK:G\n  z2|{^A}BdBG- G4 |z ^DEG _B A3|G8-  |G6 \\n  z2|    zEGA- A4 |z  EGA _B A3|G8-  |G6\n  z2|    FAFD  D4 |z2 _BB  B A3|G8-  |G6 \\n  z2|{^A}BdBG- GG3|z ^DEG _B A3|G8-  |G6\n  z2|    zEGA- AA3|z ^DEG _B A3|G8-  |G6 \\n  z2|    FAFD- DD3|z2 _BB  B A3|G8-  |G4||\n%\nzddd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4- A  \\n c2c|    cc3-  c4 |z2 c^c  d A3|G8-  |G4  z2\n  dd|    dd3-  d4 |z  c2^c d_B3|A8-  |A4  z2 \\n  c2|    cc2c- c4 |z2 c^c  d A3|G4 A4|d6||\n%\n(^AB)| G2(^AB) G2(AB) |G2(^AB) (GA)B  G-|G8-|G4 z2\n(^AB)|=A2  GE  G2(A_B)|A2(GE)   G(^AB)G-|G8-|G4 z2\n(^AB)| d2(^AB) D(AB)C-|C(^AB)D- D (AB)G-|G8 |z4 z2|]"
+  },
+  {
+    name: "St James Infirmary",
+    abc: "X:1\nT:St James Infirmary\nL:1/8\nM:4/4\nI:linebreak $\nK:C\nV:1 treble nm=\nV:1\n z2 z2 z2 DF |: A3 F G3 A | FD- D6 | %3\n A3 A d2 c2 | A6 DF |$ %5\n A3 A/F/ G3 A | FD- D4 z D | %7\n F3 D A2 FE | D8 :| %9"
+  },
+  {
+    name: "Frankie and Albert",
+    abc: "X:1\nT:Frankie and Albert\nM:4/4\nL:1/4\nK:F\n F3/2 G/2A/2 G/2 A| A/2 F/2- F2 z| A3/2 G/2A/2 A F/2-| F2 z2| B/2 B B/2 d d|\\n d/2 B3/2 z d/2d/2| d d c B| A F F F| G2 z2| z A3/4 G/4 A3/4- F/4 G|\\n F2 z2|"
+  },
+  {
+    name: "The House of the Rising Sun",
+    abc: "X:1\nT:The House of the Rising Sun\nC:Traditional\nM:3/4\nL:1/4\nK:F\nA | D2 (E/2D/2) | F  (G/2^G/2) | A (F/2E/2) |\nD2 A | d2 A | c (A/2G/2) | A3- |\nA2 ^c/2d/2 | d- d D | F/2 F3/2 A/2 | A/2\nA/2 A F/2E/2 | D2 G | A2 A |\nA2 (F/2E/2) | D2- D- | D z z | z z2 | z3 |\nz z =B ||\nK:G\nE2 F/2E/2 | G/2 G3/2 A/2^A/2 | B2 G/2A/2 ||\nE2 B | e2 B | d2 B/2A/2 | B3- | B2 ^d/2e/2 |\ne- e E | g G B | B/2B/2 B G/2F/2 |\nE2  A | B2 B | B2 G/2F/2 | E2- E- | E2 A\n |\nB2 B/2B/2 | B2 G/2F/2 | E2- E- | E2 z :|"
+  },
+  {
+    name: "Careless Love",
+    abc: "X:1\nT:Careless Love\nM:C|\nL:1/4\nQ:1/2=80\nK:F\nA3 F|E C E G|F4-  |F4||\\nA3 B|c c d c|G4-  |G4||\nA3 B|c3   ^c|d2 B2|F4||\\nA3 F|E C E G|F4-  |F4|]"
+  },
+  {
+    name: "Just A Closer Walk With Thee",
+    abc: "X:1\nT:Just A Closer Walk With Thee\nM:4/4\nL:1/8\nK:C\n G3 ^G A/2 E3/2 =G^F| =F8| d3 d B/2 G3/2 AB| A/2- G3/2- G6| e3 g e/2 c3/2 dc|\\n c6 cA| G3 G G2 G/2 G3/2| G8|"
+  },
+  {
+    name: "Georgia on my Mind",
+    abc: "X:1\nT:Georgia on my Mind\nL:1/4\nM:4/4\nK:F\nA c3|A G3|z A d A|G3F/G/|\nAc ed|Bd AG|c2-c2|d/d/d/d/ ^G2|\nA c3|A G3|z A d A|G3F/G/|\nAc ed|BD AA|F2-F2-|FF z2|\nDF GA|FD F2|DF GA|cA =B2|\nDF GA|cde>d|cA cA|A2 ^G2|\nA c3|A G3|z A d A|G3F/G/|\nAc ed|BD AA|F4-|F2 z2|]"
+  },
+  {
+    name: "la Vie en Rose",
+    abc: "X:1\nT: la Vie en Rose\nC: Louiguy\nM: C\nL: 1/8\nK: C\n|c3 B AGEc | B3 A GECB | A3 G EB,CB | [A4F4] [G4B,4] |\nd3 c BAFc | B3 A GFDB | A3 G F^C DB | A4 G2-G2 |\nc3B AGEc|B3A GECB|A3G EB,CB|c4 d3c|\ndd2c dd2c | dd2c G4 | dd2c dd2c | dd2c e2 Hd2 |\nc3 B AGEc | B3 A GECB | A3G A2 B2| [cE]6 z2 |]"
+  }
+];
