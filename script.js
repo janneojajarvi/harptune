55f53de7cc6d78d611f30a172e93eb58eb99ebde
janneojajarvi <janne.ojajarvi@gmail.com>
2026-06-27 11:51:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -569,7 +569,7 @@ const defaultHarpMap = harpMap.richter;
     isMinor: true,
     tuning: 'harmonicMinor',
     octaveShift: 0,
-    noteOffset: 0
+    noteOffset: 6
 },
     
     // Paddy Richterit (val = 12 on C, 2 on D, 7 on G)
